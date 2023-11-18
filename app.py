dd007a41236d16f152959555ccb5cdae6ac25730
Daniel Luo <danbrosluo2011@gmail.com>
2023-11-18 15:19:11 -0500

    Added route to get recipes by food name

--- app.py
+++ app.py
@@ -2,31 +2,43 @@ from flask import Flask, render_template, request
 import requests
 from dotenv import load_dotenv
 import os
-from fractions import Fraction
 
 load_dotenv()
 
 
 app = Flask(__name__)
 
-# Spoonacula
+# Spoonacular
 SPOONACULAR = os.getenv("SPOONACULAR_API_KEY")
 
 # Spoonacular API endpoint
 SPOONACULAR_API_URL = "https://api.spoonacular.com/recipes/findByIngredients"
+SPOONACULAR_COMPLEX_SEARCH = "https://api.spoonacular.com/recipes/complexSearch"
 
+@app.route("/recipesbyfood", methods=["POST"])
+def get_recipe_by_food():
+    if request.method == "POST":
+        user_input = request.form["food_input"]
 
+        params = {
+            "apiKey": SPOONACULAR, # always required
+            "query": user_input,
+        }
 
-def format_number(number):
-    # Convert to Fraction and simplify
-    fraction = Fraction(number).limit_denominator()
-
-    # If the result is a whole number, return it as an integer
-    if fraction.denominator == 1:
-        return str(fraction.numerator)
-    else:
-        # If the result is a fraction, return it in the form "numerator/denominator"
-        return f"{fraction.numerator}/{fraction.denominator}"
+        response = requests.get(SPOONACULAR_COMPLEX_SEARCH, params=params) # get recipes by food
+        if response.status_code == 200:
+            recipe_names = response.json()["results"] # id, foodname, image, imageType
+            recipe_ids = list(map(lambda r: r['id'], recipe_names))
+            recipe_ingredients = [requests.get(f"https://api.spoonacular.com/recipes/{ID}/ingredientWidget.json",
+                                               {"apiKey": SPOONACULAR, "id": ID}).json() for ID in recipe_ids]
+            name_by_ingredients = [{**recipe_id, **recipe_ingredient}
+                                 for (recipe_id, recipe_ingredient) in zip(recipe_names, recipe_ingredients)]
+            print(name_by_ingredients)
+            return render_template("recipesbyfood.html", recipes=name_by_ingredients) # passed arguments MUST be jsons
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return f"Error: {response.status_code} - {response.text}"
+        
 
 
 @app.route("/")
@@ -37,12 +49,12 @@ def index():
 @app.route("/recipes", methods=["POST"])
 def get_recipes():
     if request.method == "POST":
-        user_input = request.form["food_input"]
+        user_input = request.form["ingredients_input"]
         print(f"user_input: '{user_input}'")
 
         # Call Spoonacular API to get recipes based on user input
         params = {
-            "apiKey": SPOONACULAR,  # always required
+            "apiKey": SPOONACULAR, # always required
             "ingredients": user_input,
         }
 
