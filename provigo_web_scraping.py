3363e38852ced9f8b5804208c2aabc0e044a1388
Rain <raina.hu@mail.mcgill.ca>
2023-11-18 11:01:45 -0500

    Get url from search query then get info from url PROVIGO done

--- provigo_web_scraping.py
+++ provigo_web_scraping.py
@@ -1,11 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
 # url = "https://www.provigo.ca/concombres-anglais/p/20070132001_EA"
-url = "https://www.provigo.ca/carottes-sac-de-3-lb/p/20600927001_EA"
+# url = "https://www.provigo.ca/carottes-sac-de-3-lb/p/20600927001_EA"
 
 def get_info(url):
     driver = webdriver.Chrome()
@@ -31,27 +34,25 @@ def get_info(url):
 
     if price:
         price_text = price.get_text(strip=True)
-        print("Extracted Price:", price_text)
+        #print("Extracted Price:", price_text)
     else:
         price_text = "-1"
         print("Price not found.")
 
     if quantity:
         quantity_text = quantity.get_text(strip=True)
-        print("Extracted Quantity:", quantity_text)
+        #print("Extracted Quantity:", quantity_text)
     else:
         quantity_text = "-1"
-        print("Quantity not found.")
+        #print("Quantity not found.")
 
-    print("Url: ", url)
-
+    #print("Url: ", url)
+    #TODO: convert price from string to float, figure out quantity business
     #{"Cucumber": {"Price": " 1,99", "Quantity": "1", "url": "cucumber.com"}, "Potato":{"Price": 6, "Quantity": "100g", "url": "potato.com"}}
     info = dict(Price = price_text, Quantity = quantity_text, Url = url)
     info = {name_text: info}
     print(info)
     return info
-
-# get_info(url)
 
 #Write function that can search for a certain produce on provigo website
 def get_url(name_produce):
@@ -60,48 +61,53 @@ def get_url(name_produce):
     '''
     search_url = f"https://www.provigo.ca/search?search-bar={name_produce}"
     driver = webdriver.Chrome()
+    driver.get(search_url)
 
-    try:
-        driver.get(search_url)
+    # Wait for the page to load (adjust the wait time as needed)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "product-tile__details__info__name__link"))
+    )
 
-        # Wait for the page to load (adjust the wait time as needed)
-        driver.implicitly_wait(20)
+    # Find the element you want to wait for (assuming it has a class named 'revealed')
+    revealed = driver.find_element(By.CLASS_NAME, 'product-tile__details__info__name__link')
 
-        page_source = driver.page_source
-        soup = BeautifulSoup(page_source, 'html.parser')
+    wait = WebDriverWait(driver, timeout=2)
+    wait.until(lambda d : revealed.is_displayed())
 
-        # #Put soup into a txt file, good for debugging hehe
-        html = soup.prettify()  #soup is your BeautifulSoup object
-        with open("out.txt","w") as out:
-            for i in range(0, len(html)):
-                try:
-                    out.write(html[i])
-                except Exception:
-                    1+1
+    html = driver.page_source
+    # time.sleep(30) #Keeps browser open
+    driver.quit()
 
-        #Triage by cheapest to most expensive and then grab cheapest item
-        # dropdown = driver.find_element(By.CLASS_NAME, "MuiSvgIcon-root styled-dropdown__selected-item__icon")
-        # dropdown.click
-        # button = driver.find_element(By.CSS_SELECTOR, "button[data-option-value='price-asc']")
-        # button.click
-        # print("WOOT")
-        # link = soup.find('a', class_ ="product-tile__details__info__name__link")['href']
-        # print(link)
-        # link_element = soup.find('a', class_='product-tile__details__info__name__link')
+    soup = BeautifulSoup(html, 'html.parser')
 
-        # # Extract the href attribute
-        # if link_element:
-        #     href_link = link_element.get('href')
-        #     print("Href link:", href_link)
-        # else:
-        #     print("No matching element found.")
+    link = soup.find("a", class_="product-tile__details__info__name__link")['href']
+    link = "https://www.provigo.ca"+link
+    print(link)
+    return link
 
-        # return link
 
-    finally:
-        driver.quit()
+###Use case: Get info for "saumon"
+url = get_url("patate")
+get_info(url)
 
-get_url("patate")
+#Triage by cheapest to most expensive and then grab cheapest item
+# dropdown = driver.find_element(By.CLASS_NAME, "MuiSvgIcon-root styled-dropdown__selected-item__icon")
+# dropdown.click
+# button = driver.find_element(By.CSS_SELECTOR, "button[data-option-value='price-asc']")
+# button.click
+# print("WOOT")
+# link = soup.find('a', class_ ="product-tile__details__info__name__link")['href']
+# print(link)
+# link_element = soup.find('a', class_='product-tile__details__info__name__link')
+
+# # Extract the href attribute
+# if link_element:
+#     href_link = link_element.get('href')
+#     print("Href link:", href_link)
+# else:
+#     print("No matching element found.")
+
+# return link
 
 # #Put soup into a txt file, good for debugging hehe
 # html = soup.prettify()  #soup is your BeautifulSoup object
